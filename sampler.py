24e62788fac283b189d31351218a192753fea057
Ved Basu <ved.basu@gmail.com>
2016-10-24 07:13:08 -0400

    Testing one-step distribution

--- sampler.py
+++ sampler.py
@@ -1,5 +1,5 @@
 '''
-An HMC sampler for mirt_util.py in conditional_energy_data
+An HMC sampler for conditional_energy_data in mirt_util.py
 '''
 
 from __future__ import division
@@ -7,35 +7,33 @@ import numpy as np
 import copy as cp
 
 def main():
-    '''
-    q = np.array([[-1.50,-1.55]])
-    p = np.array([[-1,1]])
 
-    q = q.T
-    p = p.T
-    '''
-    q = 0.5
-    p = -0.5
+    #hyperparameters
+    num_samples = 500
+    eps = 0.75 #leapfrog step size
+    L = 1000 #leapfrog steps per iteration
+
+    #initial conditions
+    q_init = 0.5
+    #p = -0.5
 
     def U(q):
         return q**2 / 2
     def grad_U(q):
         return q
 
-    sample_chain = [q]
-    for _ in xrange(500):
-        q = HMC(U, grad_U, 0.75, 500, q, 1)
-        sample_chain.append(np.asscalar(q))
-        #print "sample:", np.asscalar(q)
+    sample_chain = []
+    for _ in xrange(num_samples):
+        q_new = HMC(U, grad_U, eps, L, q_init, 1)
+        sample_chain.append(float(q_new))
 
-    mean = sum(sample_chain)/len(sample_chain)
-    print "Mean:", mean
+    print "HMC mean:", mean(sample_chain)
+    print "HMC variance:", variance(sample_chain)
 
-    var = 0
-    for s in sample_chain:
-        var += (s-mean)**2
-    var /= len(sample_chain)
-    print "variance", var
+    ideal_chain = np.random.normal(0, 1, num_samples)
+
+    print "mean:", mean(ideal_chain)
+    print "variance:", variance(ideal_chain)
 
 def HMC(U, grad_U, epsilon, L, current_q, dim):
     q = cp.copy(current_q)
@@ -45,7 +43,6 @@ def HMC(U, grad_U, epsilon, L, current_q, dim):
     p -= epsilon * grad_U(q)/2
 
     for i in xrange(L):
-        #print "q, current_q", q, current_q
         q += epsilon*p
         if i < L-1:
             p -= epsilon*grad_U(q)
@@ -53,21 +50,30 @@ def HMC(U, grad_U, epsilon, L, current_q, dim):
     p -= epsilon*grad_U(q)/2
     p = -p
 
-    #print "current_q, q", current_q, q
-    #print "current_p, p", current_p, p
     current_U = U(current_q)
     current_K = np.asscalar(np.dot(current_p, current_p.T)) / 2
     proposed_U = U(q)
     proposed_K = np.asscalar(np.dot(p, p.T)) / 2
 
-    #print "current U,K", current_U, current_K
-    #print "proposed U,K", proposed_U, proposed_K
     unif_random = np.random.uniform()
-    #print "result", np.exp((current_U+current_K) - (proposed_U+proposed_K))
     if np.exp((current_U+current_K) - (proposed_U+proposed_K)) > unif_random:
         return q
     else:
         return current_q
+
+def mean(samples):
+    if len(samples) == 0: return 0
+    return sum(samples)/len(samples)
+
+def variance(samples):
+    if len(samples) == 0: return 0
+    m = mean(samples)
+    var = 0
+    for s in samples:
+        var += (s-m)**2
+    var /= len(samples)
+    return var
+
 
 if __name__ == "__main__":
     main()
